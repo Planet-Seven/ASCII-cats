6f10aceb445e7c0892fb2995aadfb3e1a347da33
Vojtech Svoboda <svobov25@fit.cvut.cz>
2026-02-02 20:20:17 +0800

    get arguments

--- src/main/scala/views/REST/RESTView.scala
+++ src/main/scala/views/REST/RESTView.scala
@@ -2,24 +2,33 @@ package views.REST
 
 import cask.*
 import converters.ASCIIConverterImpl
-import exporters.{StringExporterSubscriber, StringExporter}
+import exporters.{StringExporter, StringExporterSubscriber}
+import filters.BrightnessFilter
 import generator.{ASCIIArtGenerator, GeneratorArguments}
 import loaders.CatAPILoader
 
 object RESTView extends MainRoutes {
-  @cask.post("/cat")
-  def post(request: Request): String = {
+  @cask.get("/cat")
+  def get(width: Option[Int] = None,
+          brightness: Option[Int] = None,
+          invert: Option[Boolean] = None,
+          conversionTable: Option[String] = None,
+          customTable: Option[String] = None): String = {
 
     val result: StringExporterSubscriber = StringExporterSubscriber()
     val exporter: StringExporter = StringExporter()
     exporter.subscribe(result)
 
     val generatorArguments = GeneratorArguments(
-      _imageLoader = CatAPILoader(),
+      _imageLoader = CatAPILoader(width.getOrElse(120)),
       _filters = Seq(),
       _ascii_converter = ASCIIConverterImpl(),
       _exporters = Seq(exporter)
     )
+
+    brightness match
+      case Some(value) => generatorArguments.filters = generatorArguments.filters :+ BrightnessFilter(value/255.0)
+      case None =>
 
     val generator = ASCIIArtGenerator()
     generator.execute(generatorArguments)
